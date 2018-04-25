b4dcad621ccfa767061a0f8197133d2ba7a2d29d
unknown <tpham20908@gmail.com>
2018-04-25 18:31:10 -0400

    create AlbumController

---
+++ MVCMusicStore/MVCMusicStore/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMusicStore.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+    }
+}
